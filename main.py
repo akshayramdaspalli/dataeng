1aa5324fb6faf3256c433330dec90f1ffd5a84bb
akshayramdaspalli <akshay.ramdaspallii@gmail.com>
2024-03-15 17:00:07 +0530

    added _path for 'ratings.csv' file
    edited the code to import ratings data into the database

--- main.py
+++ main.py
@@ -14,8 +14,10 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     file_path = "E:\\Books.csv"
+    file_path2="E:\\Ratings.csv"
     df = pd.read_csv(file_path, encoding="utf-8")
     print(df)
+    df2=pd.read_csv(file_path2,low_memory=False,encoding="utf-8")
     conn_str = "mysql+mysqlconnector://hanoomac_dataeng:LetsD0C0nnect@txpro1.fcomet.com:3306/hanoomac_dataeng"
     print("connection string set")
 
@@ -23,7 +25,7 @@ if __name__ == '__main__':
     engine = create_engine(conn_str, echo=True)
 
     df.to_sql('BOOKS', engine, if_exists='replace')
-
+    df2.to_sql('Rating',engine,if_exists='replace')
     # read table data using sql query
     sql_df = pd.read_sql(
         "SELECT * FROM BOOKS",
@@ -31,5 +33,6 @@ if __name__ == '__main__':
     )
     print("Read from Table BOOKS.")
     print(sql_df)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
